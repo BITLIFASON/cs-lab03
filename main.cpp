64066d4b8d5eefcd1dbd8bf7744e06f3aa840949
BITLIFASON <LarchevVI@mpei.ru>
2020-05-15 13:29:07 +0300

    3.2.3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "histogram.h"
 #include "svg.h"
+#include <curl/curl.h>
 
 vector<double> input_numbers(istream& in, size_t count)
 {
@@ -30,6 +31,7 @@ Input read_input(istream& in, bool prompt) {
 }
 
 int main() {
+    curl_global_init(CURL_GLOBAL_ALL);
     const auto data = read_input(cin, true);
     const auto bins = make_histogram(data);
     show_histogram_svg(bins);
