a626d274c825f602273b582f103f04e0bfa2a9e0
BITLIFASON <LarchevVI@mpei.ru>
2020-05-15 12:57:58 +0300

    3.1.2.3

--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ vector<double> input_numbers(istream& in, size_t count)
     return result;
 }
 
-Input read_input(istream& in) {
+Input read_input(istream& in, bool prompt) {
     Input data;
 
-    cerr << "Enter number count: ";
+    if(prompt) cerr << "Enter number count: ";
     size_t number_count;
     cin >> number_count;
 
-    cerr << "Enter numbers: ";
+    if(prompt) cerr << "Enter numbers: ";
     data.numbers = input_numbers(in, number_count);
 
-    cerr << "Enter column count: ";
+    if(prompt) cerr << "Enter column count: ";
     size_t bin_count;
     cin >> bin_count;
     data.bin_count = bin_count;
@@ -30,7 +30,7 @@ Input read_input(istream& in) {
 }
 
 int main() {
-    const auto data = read_input(cin);
+    const auto data = read_input(cin, true);
     const auto bins = make_histogram(data);
     show_histogram_svg(bins);
     return 0;
