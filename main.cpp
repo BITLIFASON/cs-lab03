17b2d3da6f9a7d65a282ec3e812d460934e8c6d6
BITLIFASON <LarchevVI@mpei.ru>
2020-05-18 13:13:15 +0300

    3.2.1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "histogram.h"
 #include "svg.h"
+#include <windows.h>
 
 vector<double> input_numbers(size_t count)
 {
@@ -12,7 +13,9 @@ vector<double> input_numbers(size_t count)
 }
 
 int main() {
-    // Ââîä äàííûõ
+    printf("version10 = %lu\n",GetVersion());
+    printf("version16 = %08lx\n",GetVersion());
+    return 0;
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
@@ -21,7 +24,6 @@ int main() {
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
-    const auto bins = make_histogram(numbers, bin_count);    // Îáðàáîòêà äàííûõ
-    show_histogram_svg(bins);  // Âûâîä äàííûõ
-    return 0;
+    const auto bins = make_histogram(numbers, bin_count);
+    show_histogram_svg(bins);
 }
