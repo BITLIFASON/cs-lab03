29768f1a17f8eb6a12c3eb21562e032e827f081a
BITLIFASON <LarchevVI@mpei.ru>
2020-05-18 13:00:16 +0300

    исходный проект

--- histogram.h
+++ histogram.h
@@ -8,10 +8,5 @@ using namespace std;
 void find_minmax(const vector<double>& numbers, double& min, double& max);
 vector <size_t> make_histogram(const vector<double>& numbers, size_t bin_count);
 void show_histogram_text(vector <size_t>& bins);
-void svg_begin(double width, double height);
-void svg_end();
-void svg_text(double left, double baseline, string text);
-void svg_rect(double x = 0, double y = 0, double width = 100, double height = 200, string stroke = "black", string fill = "black");
-void show_histogram_svg(const vector<size_t>& bins);
 
 #endif // HISTOGRAM_H_INCLUDED
