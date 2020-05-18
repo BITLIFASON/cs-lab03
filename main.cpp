713c7e8a1b633f8ffc6065fa6f12794fc15dc171
BITLIFASON <LarchevVI@mpei.ru>
2020-05-18 13:39:42 +0300

    3.2.5

--- main.cpp
+++ main.cpp
@@ -14,8 +14,9 @@ vector<double> input_numbers(size_t count)
 
 int main() {
     DWORD info = GetVersion();
-    printf("info = %lu\n",info);
     DWORD mask = 0x0000ffff;
+    DWORD build;
+    DWORD platform = info >> 16;
     DWORD version = info & mask;
     DWORD version_major = version & 0xff;
     DWORD version_minor = version >> 8;
@@ -25,9 +26,10 @@ int main() {
     printf("m_version16 = %08lx\n",version_minor);
     if ((info & 0x80000000) == 0)
     {
-    printf("minor_bit = %u",0);
+    build = platform;
     }
     else printf("minor_bit = %u",1);
+    printf("Windows v%lu.%lu (build %lu)\n",version_major,version_minor,build);
     return 0;
     size_t number_count;
     cerr << "Enter number count: ";
